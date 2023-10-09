aa27aa5ca41235cb7e0b70a1cddf7bfd50f1b944
Artyom Filimonov <artyom.filimonov94@gmail.com>
2023-10-09 20:45:13 +0300

    third commit

--- Python/01. Py-homeworks-basic/07. Files/Homework/07. Files_3.py
+++ Python/01. Py-homeworks-basic/07. Files/Homework/07. Files_3.py
@@ -1,6 +1,20 @@
-file_names = ['1.txt', '2.txt', '3.txt']
-for file_name in file_names:
-    with open(file_name, 'r', encoding='utf8') as result:
-        lines = result.readlines()
-        length = len(lines)
-        print(length)
+from pprint import pprint
+
+def merge_files(files_list):
+    data = {}
+    for file in files_list:
+        with open(file, encoding='utf-8') as f:
+            file_data = f.readlines()
+            data[len(file_data)] = (file, ' ', file_data)
+    #pprint(data)
+    data = dict(sorted(data.items()))
+    pprint(data)
+
+    with open('result.txt', 'w', encoding= 'utf-8') as result:
+        for key, value in data.items():
+            result.write(f"{value[0]} \n")
+            result.write(f"{key} \n")
+            result.write(f"{value[1]} \n")
+
+files = ['1.txt', '2.txt', '3.txt']
+merge_files(files)
